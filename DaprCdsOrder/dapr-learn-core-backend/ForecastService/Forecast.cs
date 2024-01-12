c0583b77fc126e8bbb6c998fc7f73db6ee352aee
pumkko <c.coque@hotmail.fr>
2024-01-12 10:45:53 -0500

    create project, add efcore, add CORS (for now)

---
+++ DaprCdsOrder/dapr-learn-core-backend/ForecastService/Forecast.cs
@@ -0,0 +1,13 @@
+﻿namespace ForecastService
+{
+    public record Forecast
+    {
+        public required string ArtistName { get; set; }
+
+        public required string Costume { get; set; }
+
+        public required int Month { get; set; }
+
+        public required int ForecastValue { get; set; }
+    }
+}
